15075292b54d9cf0f65cda7bb5756fdc7f8ffcad
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-05-23 01:39:20 +0600

    registration via api

--- djangoAuthApi/account/views.py
+++ djangoAuthApi/account/views.py
@@ -2,11 +2,17 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
+from .serializers import UserRegistrationSerializer
 
 class UserRegistrationView(APIView):
     # def get(self, request, format=None):
     
     # post --> to registration
-    def post(self, request, format=None):
+    def post(self, request, *args, **kwargs):
         serializer = UserRegistrationSerializer(data=request.data)
-        return Response({'msg': 'Registration Success'})
+        
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({'msg': 'Registration Successful'}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
