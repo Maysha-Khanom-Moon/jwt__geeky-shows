a6afd371bc72ed7d331dc408d4874bdd4d61a262
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-05-22 15:26:58 +0600

    simple jwt token setup

--- djangoAuthApi/djangoAuthApi/settings.py
+++ djangoAuthApi/djangoAuthApi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'account',
 ]
 
@@ -101,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
